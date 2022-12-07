8f93113ff84a250a6be60cf56139c4ae56a502cb
vishvak1 <vishvak.vishvak98@gmail.com>
2022-12-07 14:30:08 +0530

    Created js object 'containers' in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,29 @@
 const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+    },
+    {
+      id: 2,
+      title: "Jackets",
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+    },
+    {
+      id: 4,
+      title: "Womens",
+    },
+    {
+      id: 5,
+      title: "Mens",
+    },
+  ];
+  // Creating a javascript object to serve as a centralized singular source of data that can be referenced from anywhere in the file.
+  // This way we don't have to hardcode the data and also any change made to the 'categories' object will be reflected everywhere it's been referred to.
+
   return (
     <div className="categories-container">
       <div className="category-container">
