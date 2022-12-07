d2b9d3412153097e873811470c9f4f5b69e7ebcc
vishvak1 <vishvak.vishvak98@gmail.com>
2022-12-07 20:36:53 +0530

    Created category-item component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import "./scss/categories.styles.scss";
+import CategoryItem from "./components/category-item/category-item.component";
+import "./components/category-item/scss/categories.styles.scss";
 
 const App = () => {
   const categories = [
     {
       id: 1,
-      title: "Hats",
+      title: "hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
     },
     {
       id: 2,
-      title: "Jackets",
+      title: "jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
     },
     {
       id: 3,
-      title: "Sneakers",
+      title: "sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
     },
     {
       id: 4,
-      title: "Womens",
+      title: "womens",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
     },
     {
       id: 5,
-      title: "Mens",
+      title: "mens",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
     },
   ];
   // Creating a javascript object to serve as a centralized singular source of data that can be referenced from anywhere in the file.
@@ -28,18 +34,9 @@ const App = () => {
 
   return (
     <div className="categories-container">
-      {categories.map(({ title, id }) => {
-        return (
-          <div key={id} className="category-container">
-            {/* <img/> */}
-            <div className="background-image"></div>
-            <div className="category-body-container">
-              <h2>{title}</h2>
-              <p>Shop now</p>
-            </div>
-          </div>
-        );
-      })}
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
     </div>
 
     // .map allows you to traverse through the object and display the properties and it's values in the UI with minimal code
