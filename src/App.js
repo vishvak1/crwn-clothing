5ab82111f058fc156af3483fbbf4156572d75cdb
vishvak1 <vishvak.vishvak98@gmail.com>
2022-12-07 14:33:24 +0530

    Used .map in App.js

--- src/App.js
+++ src/App.js
@@ -26,41 +26,17 @@ const App = () => {
 
   return (
     <div className="categories-container">
-      <div className="category-container">
-        {/* <img/> */}
-        <div className="category-body-container">
-          <h2>Hats</h2>
-          <p>Shop now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img/> */}
-        <div className="category-body-container">
-          <h2>Jackets</h2>
-          <p>Shop now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img/> */}
-        <div className="category-body-container">
-          <h2>Sneakers</h2>
-          <p>Shop now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img/> */}
-        <div className="category-body-container">
-          <h2>Womens</h2>
-          <p>Shop now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img/> */}
-        <div className="category-body-container">
-          <h2>Mens</h2>
-          <p>Shop now</p>
-        </div>
-      </div>
+      {categories.map((category) => {
+        return (
+          <div className="category-container">
+            {/* <img/> */}
+            <div className="category-body-container">
+              <h2>{category.title}</h2>
+              <p>Shop now</p>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
