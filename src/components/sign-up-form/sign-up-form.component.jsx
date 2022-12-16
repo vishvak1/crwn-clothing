40218ade6b4e8a14ca92cf73370ab5dec64fb876
Vishvak V <vishvak.v@publicissapient.com>
2022-12-16 17:53:08 +0530

    removed the console statements

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -11,8 +11,6 @@ const SignUpForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { displayName, email, password, confirmPassword } = formFields;
 
-  console.log(formFields);
-
   const handleSubmit = (event) => {
     const { name, value } = event.target;
     setFormFields({ ...formFields, [name]: value });
