b12a15716e822ae7de6550316d25e8433897b9a3
Vishvak V <vishvak.v@publicissapient.com>
2022-12-21 18:26:41 +0530

    added sign-out functionality

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -4,12 +4,18 @@ import { useContext } from "react";
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 
 import { UserContext } from "../../contexts/user.context";
+import { signOutAuthUser } from "../../utils/firebase/firebase.util";
 
 import "./scss/navigation.styles.scss";
 
 const NavigationBar = () => {
-  const { currentUser } = useContext(UserContext);
-  console.log(currentUser);
+  const { currentUser, setCurrentUser } = useContext(UserContext);
+
+  const handleSignOut = async () => {
+    await signOutAuthUser();
+    setCurrentUser(null);
+  };
+
   return (
     <>
       <div className="navigation">
@@ -20,9 +26,15 @@ const NavigationBar = () => {
           <Link className="nav-link" to="/shop">
             SHOP
           </Link>
-          <Link className="nav-link" to="/auth">
-            SIGN-IN
-          </Link>
+          {currentUser ? (
+            <span className="nav-link" onClick={handleSignOut}>
+              SIGN OUT
+            </span>
+          ) : (
+            <Link className="nav-link" to="/auth">
+              SIGN-IN
+            </Link>
+          )}
         </div>
       </div>
       <Outlet />
