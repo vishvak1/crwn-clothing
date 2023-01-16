2c8a64d4c9782c66d0d8cb40c8bea0da67f4b6ad
Vishvak V <vishvak.v@publicissapient.com>
2023-01-16 21:30:56 +0530

    minor fix

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from "react";
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import { register } from "../../utils/auth/sign-up.util";
